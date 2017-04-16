5f6844fa9ab33039b884e17b1c4e737feca43271
wayn3h0 <hi@wayn3h0.com>
2017-04-17 00:04:16 +0800

    feat: add functions for Decimal type
    1. use big.Float instead of big.Int
    2. add some new helpful functions according to built-in big.Float type
        2.1 *Decimal.Copy
        2.2 *Decimal.IsZero
        2.3 *Decimal.Quo
        2.4 *Decimal.Abs
        2.5 *Decimal.Neg
        2.6 *Decimal.Float32
        2.7 *Decimal.Int64
        2.8 add rounding functions (built-in big package has problem for
    rounding):
            2.8.1 *Decimal.RoundToNearestEven
            2.8.2 *Decimal.RoundToNearestAway
            2.8.3 *Decimal.RoundToZero
            2.8.4 *Decimal.Round (same as *Decimal.RoundToNearestEven)
            2.8.5 *Decimal.Truncate (same as *Decimal.RoundToZero)
    
    BREAKING CHANGE: some fucntions has changed or removed.
    
    Changed:
    
        Before: *Decimal.SetString(string) (*Decimal, error)
        After : *Decimal.SetString(string) (*Decimal, bool)
    
        Before: *Decimal.Float64() float64
        After : *Decimal.Float64() (float64, bool)
    
    Removed:
    
        *Decimal.FloatString(uint) string

--- big/doc.go
+++ big/doc.go
@@ -2,5 +2,9 @@
 
 Package big providers utilities for big numbers.
 
+* NOTE: built-in big.Float rounding returns unpredicatable result.
+		(bugs? example: number 2.34555 round to nestest even got 2.3455).
+	    So we providers rounding functions.
+
 */
 package big
